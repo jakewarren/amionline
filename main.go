0ad05e05cdaee5dac5981ce02a9b9d1a2ce2f528
Jake Warren <jakewarren@users.noreply.github.com>
2018-03-28 10:57:15 -0500

    add verbose and version output

--- main.go
+++ main.go
@@ -1,18 +1,21 @@
 package main
 
 import (
-	"net"
-	"os"
 	"flag"
 	"fmt"
+	"net"
 	"net/http"
+	"os"
 	"regexp"
+
 	"github.com/pkg/errors"
 )
 
-type Config struct{
-	dns bool
-	http bool
+var version string
+
+type Config struct {
+	dns    bool
+	http   bool
 	domain string
 }
 
@@ -20,51 +23,57 @@ func main() {
 
 	var app Config
 
-	flag.BoolVar(&app.dns,"dns",false,"check dns resolution")
-	flag.BoolVar(&app.http,"http",false,"check url for 200 status code")
-	flag.StringVar(&app.domain,"domain","httpbin.org","domain name to query")
+	flag.BoolVar(&app.dns, "dns", false, "check dns resolution")
+	flag.BoolVar(&app.http, "http", false, "check url for 200 status code")
+	flag.StringVar(&app.domain, "domain", "httpbin.org", "domain name to query")
+	displayVersion := flag.Bool("version", false, "display version")
+	verbose := flag.Bool("verbose", false, "verbose output")
 	flag.Parse()
 
-    var err error
+	if *displayVersion {
+		fmt.Println(version)
+		os.Exit(0)
+	}
 
-    if app.dns{
-    	err = app.dnsCheck()
-	}else if app.http {
+	var err error
+
+	if app.dns {
+		err = app.dnsCheck()
+	} else if app.http {
 		err = app.httpCheck()
 	}
 
 	if err != nil {
-		fmt.Println(err)
+		if *verbose {
+			fmt.Println(err)
+		}
 		os.Exit(1)
 	} else {
 		os.Exit(0)
 	}
 }
 
-func (c *Config) dnsCheck() error{
+func (c *Config) dnsCheck() error {
 	_, err := net.LookupHost(c.domain)
 	return err
 }
 
-func (c *Config) httpCheck() error{
+func (c *Config) httpCheck() error {
 	domain := c.domain
 
-
-	if ok,_ := regexp.MatchString(`https?://`,domain);!ok{
-		domain = "https://"+domain
+	if ok, _ := regexp.MatchString(`https?://`, domain); !ok {
+		domain = "https://" + domain
 	}
-
 
 	resp, err := http.Get(domain)
 	if err != nil {
 		return err
 	}
 
-
 	if resp.StatusCode >= 200 && resp.StatusCode <= 299 {
 		return nil
 	} else {
-		return errors.New("Invalid status code")
+		return errors.New(fmt.Sprintf("%s (%d)", "Invalid status code", resp.StatusCode))
 	}
 
 }
