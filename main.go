75471e6aa658ffa9d29f49344646f80b73419848
Jake Warren <jakewarren@users.noreply.github.com>
2018-06-11 15:22:52 -0500

    add timeout to the http check

--- main.go
+++ main.go
@@ -9,14 +9,16 @@ import (
 	"regexp"
 
 	"github.com/pkg/errors"
+	"time"
 )
 
 var version string
 
 type Config struct {
-	dns    bool
-	http   bool
-	domain string
+	dns     bool
+	http    bool
+	domain  string
+	timeout time.Duration
 }
 
 func main() {
@@ -26,6 +28,8 @@ func main() {
 	flag.BoolVar(&app.dns, "dns", false, "check dns resolution")
 	flag.BoolVar(&app.http, "http", false, "check url for 200 status code")
 	flag.StringVar(&app.domain, "domain", "httpbin.org", "domain name to query")
+	flag.DurationVar(&app.timeout, "timeout", time.Second*30, "http timeout")
+
 	displayVersion := flag.Bool("version", false, "display version")
 	verbose := flag.Bool("verbose", false, "verbose output")
 	flag.Parse()
@@ -65,7 +69,18 @@ func (c *Config) httpCheck() error {
 		domain = "https://" + domain
 	}
 
-	resp, err := http.Get(domain)
+	req, err := http.NewRequest("GET", domain, nil)
+	if err != nil {
+		return err
+	}
+
+	httpClient := &http.Client{
+		Timeout: c.timeout,
+	}
+	req.Header.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36")
+	req.Header.Add("Accept-Language", "en-US,en;q=0.8")
+
+	resp, err := httpClient.Do(req)
 	if err != nil {
 		return err
 	}
